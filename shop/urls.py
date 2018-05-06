5704ca4b1623d60e95fd539f6560444cb03eec19
makhloufi <makhloufi200@gmail.com>
2018-05-06 14:01:33 +0100

    add product list function

--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('/<slug>/', views.product_list_by_category, name='product_list_by_category'),
+    #path('/<slug>/', views.product_list_by_category, name='product_list_by_category'),
+    path('/<slug>/', views.product_list, name='product_list_by_category'),
     path('/products/<slug>/', views.product_detail, name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
-def product_list(request):
+#def product_list(request):
+#    products = Product.objects.all()
+#    categorys = Category.objects.all()
+#    return render(request, 'shop/product/list.html', {'products': products, 'categorys': categorys})
+
+def product_list(request, slug=None):
     products = Product.objects.all()
     categorys = Category.objects.all()
-    return render(request, 'shop/product/list.html', {'products': products, 'categorys': categorys})
+    if slug:
+        category = get_object_or_404(Category, slug=slug)
+        template = 'shop/product/list_by_category.html'
+        context = {'category': category}
+    else:
+        template = 'shop/product/list.html'
+        context = {'products': products, 'categorys': categorys}
+    return render(request, template, context)
 
 
 def product_detail(request, slug):
@@ -13,6 +25,6 @@ def product_detail(request, slug):
     return render(request, 'shop/product/detail.html', {'product': product})
 
 
-def product_list_by_category(request, slug):
-    category = get_object_or_404(Category, slug=slug)
-    return render(request, 'shop/product/list_by_category.html', {'category': category})
+#def product_list_by_category(request, slug):
+#    category = get_object_or_404(Category, slug=slug)
+#    return render(request, 'shop/product/list_by_category.html', {'category': category})
